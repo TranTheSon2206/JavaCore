4a86783ca57a11abf1ce326641c69587c7ea84aa
TranTheSon2206 <sonttth2206024@fpt.edu.vn>
2024-04-24 10:22:14 +0700

    slot6

---
+++ shop/Category.java
@@ -0,0 +1,13 @@
+
+package shop;
+
+public enum Category {
+    FOOD ("Thuc Pham"),HOUSEWARE ("DO GIA DUNG"), COSMETICS ("MY PHAM"), FASHTION ("THOI TRANG");
+    private String value;
+    private Category(String value){
+        this.value =  value;
+    }
+    public String getValue(){
+        return value;
+    }
+}
---
+++ shop/Menu.java
@@ -0,0 +1,26 @@
+package shop;
+
+import java.util.Scanner;
+import java.util.SortedMap;
+
+public class Menu {
+    public static void mainMenu(){
+        System.out.println("1. Xem thong tin san pham ");
+        System.out.println("2. In danh sach san pham tren 10.000");
+        System.out.println("3. Dem so san pham co so luong tu 50 tro len");
+        System.out.println("4. Liet ke san pham dua tren ,loai san pham");
+        System.out.println("5. Sap sep san pham theo so luong ban duoc");
+        System.out.println("6. Dua ra san pham duoc ban nhieu nhat");
+        System.out.println("7. Sap sep san pham theo ten");
+        System.out.println("0. Thoat");
+        System.out.println("Nhap lua tron cua ban");
+    }
+
+
+    public static void secondMenu(){
+        System.out.println("1. Sua thong tin san pham");
+        System.out.println("2. Xoa san pham ");
+        System.out.println("0. Thoat");
+        System.out.println("Nhap lua tron cua ban");
+    }
+}
